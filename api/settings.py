67eb7f2a970bb88d9936f71e829982ac99516722
Endys-Hub <ndudiokehi@gmail.com>
2025-08-28 13:23:41 +0100

    Settings update

--- api/settings.py
+++ api/settings.py
@@ -1,18 +1,17 @@
 import os
 from pathlib import Path
 from decouple import config
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# from dotenv import load_dotenv
-# load_dotenv()
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vs36ui6^l#^52ca7sk%(br#sx1@u05vs2s+bq_#x6%kmvbc))2'
+SECRET_KEY = str(os.getenv("SECRET_KEY"))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
